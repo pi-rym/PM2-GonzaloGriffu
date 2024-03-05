54b1588dc49ba6c935c28df62aa138ace6ddfcda
gonzalogriffu <gonzalogriffouliere@gmail.com>
2024-03-04 23:09:28 -0300

    04.03.2024

--- front/scripts/tempData.js
+++ front/scripts/tempData.js
@@ -30,3 +30,35 @@ const tempData = [
       "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg",
   },
 ];
+const container = document.querySelector('#peliculas-container');
+
+tempData.forEach(pelicula => {
+
+  const card = document.createElement('div');
+  card.classList.add('card');
+
+  const img = document.createElement('img');
+  img.src = pelicula.poster;
+  img.alt = pelicula.title;
+
+  const title = document.createElement('h2');
+  title.textContent = pelicula.title;
+
+  const director = document.createElement('p');
+  director.textContent = `Director: ${pelicula.director}`;
+
+  const genre = document.createElement('p');
+  genre.textContent = `Género: ${pelicula.genre.join(', ')}`;
+
+  const rate = document.createElement('p');
+  rate.textContent = `Rate: ${pelicula.rate}`;
+
+
+  card.appendChild(img);
+  card.appendChild(title);
+  card.appendChild(director);
+  card.appendChild(genre);
+  card.appendChild(rate);
+
+  container.appendChild(card);
+});
